baf496010aac52fb35b6c2f2caf4e746388c8a4e
Basile <basile.desjuzeur@hec.edu>
2026-05-20 14:19:45 +0200

    railway deployment

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import logging, json
+from datetime import datetime
 
 app = Flask(__name__)
+#logging.basicConfig(filename='logs.txt', level=logging.INFO)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/contact", methods=["POST"])
+def contact():
+    data = request.form.to_dict()
+    data["ts"] = datetime.now().isoformat()
+    #logging.info(json.dumps(data, ensure_ascii=False))
+    return render_template("index.html", success=True)
