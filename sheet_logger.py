363b0c7e8d1776c3843d1516f7275b51e09b758c
Basile <basile.desjuzeur@hec.edu>
2026-06-04 13:05:58 +0200

    feat : log via gsheets also

--- sheet_logger.py
+++ sheet_logger.py
@@ -10,7 +10,10 @@ COLUMNS = ["ts", "nom", "prenom", "email", "telephone", "objet", "message"]
 
 
 def log_to_sheet(data: dict):
-    creds = json.loads(os.getenv("GSHEET_CREDENTIALS"))
-    gc = gspread.service_account_from_dict(creds)
+    path = os.getenv("GSHEET_CREDENTIALS_PATH")
+    if path:
+        gc = gspread.service_account(filename=path)
+    else:
+        gc = gspread.service_account_from_dict(json.loads(os.getenv("GSHEET_CREDENTIALS")))
     ws = gc.open_by_key(os.getenv("GSHEET_ID")).sheet1
     ws.append_row([data.get(col, "") for col in COLUMNS])
